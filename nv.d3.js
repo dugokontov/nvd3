b4d1cfb9529b1fd88f74aae3df2db1c778a54da6
Boris Ostojic <sergejspam@gmail.com>
2013-06-13 15:27:11 +0200

    Compiled nv.d3.js wit fix

--- nv.d3.js
+++ nv.d3.js
@@ -6332,7 +6332,7 @@ nv.models.multiBar = function() {
               return y((stacked ? d.y1 : 0));
             })
             .attr('height', function(d,i) {
-              return Math.max(Math.abs(y(d.y + (stacked ? d.y0 : 0)) - y((stacked ? d.y0 : 0))),1);
+              return Math.max(Math.abs(y(d.y + (stacked ? d.y0 : 0)) - y((stacked ? d.y0 : 0))),0);
             })
             .each('end', function() {
               d3.transition(d3.select(this))
@@ -6358,7 +6358,7 @@ nv.models.multiBar = function() {
                           y(getY(d,i)) || 0;
               })
               .attr('height', function(d,i) {
-                  return Math.max(Math.abs(y(getY(d,i)) - y(0)),1) || 0;
+                  return Math.max(Math.abs(y(getY(d,i)) - y(0)),0) || 0;
                 });
             })
 
